6a2d0fc9bd25fb5028125279cd73208cb8c5a041
George Adams <georgeadams1995@gmail.com>
2019-05-14 17:55:36 +0100

    initial graph

---
+++ public/javascripts/app.js
@@ -0,0 +1,52 @@
+function generateGraph(data) {
+
+  var graphVersion = document.getElementById('graph').getContext('2d');
+  Chart.defaults.global.defaultFontSize = 20;
+  var myChart = new Chart(graphVersion, {
+    type: 'line',
+    data: {
+      datasets: [{
+        label: "download count",
+        data: data,
+        fill: false,
+        borderColor: 'blue'
+      }]
+    },
+    options: {
+      responsive: true,
+      scales: {
+        xAxes: [{
+          type: 'time',
+          time: {
+            tooltipFormat: 'll HH:mm',
+            unit: 'day',
+            displayFormats: {
+              'day': 'MM/DD/YYYY'
+            }
+          },
+          scaleLabel: {
+            display: true,
+            labelString: 'Date'
+          }
+        }],
+        yAxes: [{
+          scaleLabel: {
+            display: true,
+            labelString: 'Downloads'
+          }
+        }]
+      }
+    }
+  });
+}
+
+let plots = []
+
+for (let plot of data) {
+  plots.push({
+    t: plot.timestamp,
+    y: plot.counter
+  })
+}
+
+generateGraph(plots)
---
+++ routes/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+const {
+  Client
+} = require('pg')
+const client = new Client({
+  user: 'readonly',
+  host: '37.153.110.144',
+  database: 'downloadStats',
+  password: 'process.env.PSQL_PASSWORD',
+  port: 5432,
+})
+
+client.connect()
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  fetchStats()
+  async function fetchStats() {
+    let queryString = "SELECT * FROM downloads";
+    client.query(queryString, (err, res2) => {
+        if (err) {
+          console.error(err)
+        } else {
+          console.log(res2.rows)
+          res.render('index', {
+            data: res2.rows
+          });
+        }
+      });
+    }
+});
+
+module.exports = router;
